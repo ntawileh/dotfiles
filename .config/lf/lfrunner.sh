d6359a195fa532d09027e0295b1a5655c431a82b
Nadim Tawileh <nadim@tawileh.com>
2023-04-05 08:43:37 -0700

    add lf

---
+++ .config/lf/lfrunner.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#env
+#echo $1
+
+mime=$(file --mime-type -b $fx)
+
+case "$mime" in
+ text/*) $EDITOR $fx;;
+ *) for f in $fx; do open $f > /dev/null 2> /dev/null & done;;
+
+esac
