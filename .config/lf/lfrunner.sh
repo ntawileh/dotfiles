b27b2471e2871e5924a890cd71d3c3a30c99d31d
Nadim Tawileh <nadim@tawileh.com>
2023-04-15 17:23:16 -0700

    lfrunner changes

--- .config/lf/lfrunner.sh
+++ .config/lf/lfrunner.sh
@@ -6,7 +6,7 @@
 mime=$(file --mime-type -b $fx)
 
 case "$mime" in
- text/*) $EDITOR $fx;;
- *) for f in $fx; do open $f > /dev/null 2> /dev/null & done;;
+ text/*) $EDITOR "$fx";;
+ *) for f in $fx; do open "$f" > /dev/null 2> /dev/null & done;;
 
 esac
