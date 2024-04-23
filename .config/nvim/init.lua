7b3f20d738bfc5cc73d0e23a808c47529680d83e
Nadim Tawileh <nadim@tawileh.com>
2024-04-22 23:19:39 -0700

    nvim stuff

---
+++ .config/nvim/init.lua
@@ -0,0 +1,18 @@
+if vim.loader then
+	vim.loader.enable()
+end
+
+_G.dd = function(...)
+	require("util.debug").dump(...)
+end
+vim.print = _G.dd
+
+require("config.lazy")
+
+require("lualine").setup({
+	options = { 
+		theme = "codedark",
+		section_separators = '', 
+		component_separators = ''
+	},
+})
