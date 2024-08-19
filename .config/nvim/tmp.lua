b59db693d26d495eb09a1668e8ef27b73b730487
Nadim Tawileh <nadim@tawileh.com>
2024-08-19 08:58:48 -0700

    remove temp files

--- .config/nvim/tmp.lua
+++
@@ -1,7 +0,0 @@
-local rgb_match = "rgb%(%s*(%d%d[%d]*)%s*,%s*(%d%d[%d]*)%s*,%s*(%d%d[%d]*)%s*%)"
-local test = "RGB( 255 , 245 , 235 )"
-
-for r, g, b in test:lower():gmatch(rgb_match) do
-	print(r, g, b)
-end
-
