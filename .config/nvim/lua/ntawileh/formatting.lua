ab2051fd19894a0f9ec3d54e2dfd431d307c68aa
Nadim Tawileh <5069918+ntawileh@users.noreply.github.com>
2024-11-14 21:55:39 -0800

    feat(nvim): formatter updates for biome

--- .config/nvim/lua/ntawileh/formatting.lua
+++ .config/nvim/lua/ntawileh/formatting.lua
@@ -1,24 +1,31 @@
 local conform = require("conform")
+
 conform.setup({
-	formatters_by_ft = {
-		javascript = { "prettier" },
-		typescript = { "prettier" },
-		javascriptreact = { "prettier" },
-		typescriptreact = { "prettier" },
-		svelte = { "prettier" },
-		css = { "prettier" },
-		html = { "prettier" },
-		json = { "prettier" },
-		yaml = { "prettier" },
-		markdown = { "prettier" },
-		graphql = { "prettier" },
-		liquid = { "prettier" },
-		lua = { "stylua" },
-		python = { "isort", "black" },
-	},
-	format_on_save = {
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 1000,
-	},
+    formatters = {
+        biome = {
+            require_cwd = true,
+        },
+    },
+    formatters_by_ft = {
+        javascript = { "prettier", "biome" },
+        typescript = { "prettier", "biome" },
+        javascriptreact = { "prettier", "biome" },
+        typescriptreact = { "prettier", "biome" },
+        astro = { "prettier", "biome" },
+        svelte = { "prettier" },
+        css = { "prettier", "biome" },
+        html = { "prettier" },
+        json = { "prettier", "biome" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        liquid = { "prettier" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+    },
+    format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+    },
 })
