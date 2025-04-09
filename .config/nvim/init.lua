98e71d59000fddddf123dc54606d03ea73663198
Nadim Tawileh <5069918+ntawileh@users.noreply.github.com>
2025-04-09 10:24:45 -0700

    chore(nvim): plugin and lazy updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,8 +26,26 @@ require("ntawileh.supermaven").register_ai_keymaps()
 
 vim.diagnostic.config({
     virtual_text = false,
+    underline = true,
+    severity_sort = true,
     float = {
         border = "rounded",
         source = true,
     },
 })
+
+-- vim.diagnostic.config({
+--     signs = { priority = 9999 },
+--     underline = true,
+--     update_in_insert = false, -- false so diags are updated on InsertLeave
+--     virtual_text = { current_line = true, severity = { min = "INFO", max = "WARN" } },
+--     virtual_lines = { current_line = true, severity = { min = "ERROR" } },
+--     severity_sort = true,
+--     float = {
+--         focusable = false,
+--         style = "minimal",
+--         border = "rounded",
+--         source = true,
+--         header = "",
+--     },
+-- })
