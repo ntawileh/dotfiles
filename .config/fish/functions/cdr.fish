b93b4265b3d686a895842974d924ab6116dcda89
Nadim Tawileh <5069918+ntawileh@users.noreply.github.com>
2025-09-04 15:09:46 -0400

    improve fish cdr function

--- .config/fish/functions/cdr.fish
+++ .config/fish/functions/cdr.fish
@@ -1,3 +1,9 @@
 function cdr -d "cd back to the root folder of the current repository"
-    cd (git rev-parse --show-toplevel)
+    set root (git rev-parse --show-toplevel 2>/dev/null)
+    if test $status -eq 0
+        cd $root
+    else
+        echo "Not in a git repository."
+        return 1
+    end
 end
