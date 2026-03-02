ae31918d32d3d23c859dd87e0d9cd97ee994518c
Nadim Tawileh <5069918+ntawileh@users.noreply.github.com>
2026-03-01 21:12:00 -0800

    add hammerspoon

---
+++ .config/hammerspoon/init.lua
@@ -0,0 +1,52 @@
+-- hyper key
+local hyper = { "alt", "ctrl", "cmd" }
+
+local switcher_chrome = hs.window.switcher.new({ "Google Chrome" }, {
+    -- Show only windows from current screen (optional)
+    showCurrentSpace = true,
+    showOtherSpaces = false,
+})
+
+hs.alert.show("Config loaded")
+hs.hotkey.bind(hyper, "\\", function()
+    hs.reload()
+end)
+
+hs.hotkey.bind(hyper, "W", function()
+    hs.execute("cd ~/.config/themes/ && ./set-next-wallpaper.sh")
+end)
+
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({})
+
+-- centralize at 80% screen size
+hs.hotkey.bind(hyper, "'", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local max = win:screen():frame()
+
+    f.x = max.x * 0.8
+    f.y = max.y * 0.8
+    f.w = max.w * 0.8
+    f.h = max.h * 0.8
+
+    win:setFrame(f)
+    win:centerOnScreen()
+end)
+
+hs.hotkey.bind(hyper, "T", function()
+    hs.application.launchOrFocus("Ghostty")
+end)
+
+hs.hotkey.bind(hyper, "S", function()
+    hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bind(hyper, "N", function()
+    hs.application.launchOrFocus("Obsidian")
+end)
+
+hs.hotkey.bind(hyper, "B", function()
+    hs.application.launchOrFocus("Google Chrome")
+    switcher_chrome:next()
+end)
